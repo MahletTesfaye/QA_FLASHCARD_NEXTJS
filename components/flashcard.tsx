a784495c4435c2d4825725bab7e6a50f836751d4
MahletTesfaye <106257780+MahletTesfaye@users.noreply.github.com>
2024-04-08 12:14:40 +0300

    simplifying the code and adding responsiveness

--- components/flashcard.tsx
+++ components/flashcard.tsx
@@ -6,11 +6,7 @@ import { HiSpeakerWave } from "react-icons/hi2";
 import { RiQuestionnaireFill } from "react-icons/ri";
 import { SiAnswer } from 'react-icons/si';
 
-// type props = {
-//     question: string
-//     answer: string
-//     showHide: string
-// }
+
 const Flashcard = (props: any) => {
     const show = "Show Answer"
     const hide = "Hide Answer"
@@ -24,7 +20,7 @@ const Flashcard = (props: any) => {
         setIsLiked((prevIsLiked) => (!prevIsLiked))
     }
     return (
-        <div className="flex flex-col duration-500 hover:scale-105">
+        <div className="flex flex-col duration-500 hover:scale-105 cursor-pointer items-center">
             <div onClick={handleFlip} className={`card relative ${isFlipped ? 'cardFlip' : ''}`}>
                 <div className="card-front absolute top-0">
                     <div className="border shadow-md hover:shadow-lg h-96 w-56 pt-3 px-3 mb-1 mt-6 rounded-2xl bg-white">
@@ -45,9 +41,9 @@ const Flashcard = (props: any) => {
                     </div>
                 </div>
             </div>
-            <div className="flex justify-around cursor-pointer">
-                <div onClick={handleFlip} className="border h-8 text-center text-white px-10 rounded-lg bg-[var(--backgroundPrimary)]">{isFlipped ? hide : show}</div>
-                <BiLike size={27} onClick={handleLike} color={isLiked? 'red':'inherit'} className='mt-1'/>
+            <div className="flex cursor-pointer">
+                <div onClick={handleFlip} className="border h-8 text-center text-white px-10  rounded-lg bg-[var(--backgroundPrimary)]">{isFlipped ? hide : show}</div>
+                <BiLike size={27} onClick={handleLike} color={isLiked ? 'red' : 'inherit'} className='mt-1' />
             </div>
         </div>
     )
