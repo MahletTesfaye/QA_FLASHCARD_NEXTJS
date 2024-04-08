98001e9882661a3177f32d393ff49128647f48ea
MahletTesfaye <mahlettenaye@gmail.com>
2024-04-08 10:13:32 +0300

    version1

--- app/page.tsx
+++ app/page.tsx
@@ -3,66 +3,10 @@ import { useState } from "react";
 import { PiShuffleAngularBold } from "react-icons/pi";
 import Flashcard from "@/components/flashcard";
 
-const data = [
-  {
-    id: 0,
-    question: "What is your name?",
-    answer: "My name is paul"
-  },
-  {
-    id: 1,
-    question: "What is your father's name?",
-    answer: "My name father's is gb"
-  },
-  {
-    id: 2,
-    question: "What is your pets name?",
-    answer: "My pets name is pp"
-  },
-  {
-    id: 3,
-    question: "What is your pets name?",
-    answer: "My pets name is pp"
-  },
-  {
-    id: 4,
-    question: "What is your pets name?",
-    answer: "My pets name is pp"
-  },
-  {
-    id: 5,
-    question: "What is your pets name?",
-    answer: "My pets name is pp"
-  },
-  {
-    id: 6,
-    question: "What is your pets name?",
-    answer: "My pets name is pp"
-  },
-  {
-    id: 7,
-    question: "What is your pets name?",
-    answer: "My pets name is pp"
-  },
-  {
-    id: 8,
-    question: "What is your pets name?",
-    answer: "My pets name is pp"
-  },
-  {
-    id: 9,
-    question: "What is your pets name?",
-    answer: "My pets name is pp"
-  },
-  {
-    id: 10,
-    question: "What is your pets name?",
-    answer: "My pets name is pp"
-  }
-]
 
 export default function Home() {
   const [isLoadMore, setisLoadMore] = useState(false)
+
   const handleLoadMore = () => {
     setisLoadMore(!isLoadMore)
   }
@@ -74,19 +18,46 @@ export default function Home() {
           <div className="">From articles...</div>
           <PiShuffleAngularBold size={18} />
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-around gap-7 mx-10">
-          {
-            data.map((item: any, index: number) => {
-              return <>
-                {
-                  (isLoadMore ? item : index < 6) && <Flashcard key={item.id} question={item.question} answer={item.answer} />
-                }
-              </>
-            })
-          }
+
+        <div className="flex justify-around gap-7 mx-10">
+          <Flashcard question="who are you?" answer="shega" />
+          <Flashcard question="What are the key characteristics of a successful startup?" answer="shega" />
+          <Flashcard question="who are you?" answer="shega" />
         </div>
-        <div onClick={handleLoadMore} className={`text-center pt-4 cursor-pointer opacity-80 ${isLoadMore ? 'hidden' : ''}`}>Load more...</div>
-        <div onClick={handleLoadMore} className={`text-center pt-4 cursor-pointer opacity-80 ${isLoadMore ? '' : 'hidden'}`}>Show less...</div>
+
+        <div className="flex justify-around gap-7 mx-10">
+          <Flashcard question="What are the key characteristics of a successful startup?" answer="shega" />
+          <Flashcard question="who are you?" answer="shega" />
+          <Flashcard question="What are the key characteristics of a successful startup?" answer="shega" />
+        </div>
+        <div className={`flex justify-around gap-7 mx-10 ${isLoadMore ? '' : 'hidden'}`}>
+          <Flashcard question="who are you?" answer="shega" />
+          <Flashcard question="What are the key characteristics of a successful startup?" answer="shega" />
+          <Flashcard question="who are you?" answer="shega" />
+        </div>
+        <div className={`flex justify-around gap-7 mx-10 ${isLoadMore ? '' : 'hidden'}`}>
+          <Flashcard question="who are you?" answer="shega" />
+          <Flashcard question="What are the key characteristics of a successful startup?" answer="shega" />
+          <Flashcard question="who are you?" answer="shega" />
+        </div>
+        <div className={`flex justify-around gap-7 mx-10 ${isLoadMore ? '' : 'hidden'}`}>
+          <Flashcard question="who are you?" answer="shega" />
+          <Flashcard question="What are the key characteristics of a successful startup?" answer="shega" />
+          <Flashcard question="who are you?" answer="shega" />
+        </div>
+        <div className={`flex justify-around gap-7 mx-10 ${isLoadMore ? '' : 'hidden'}`}>
+          <Flashcard question="who are you?" answer="shega" />
+          <Flashcard question="What are the key characteristics of a successful startup?" answer="shega" />
+          <Flashcard question="who are you?" answer="shega" />
+        </div>
+        <div className={`flex justify-around gap-7 mx-10 ${isLoadMore ? '' : 'hidden'}`}>
+          <Flashcard question="who are you?" answer="shega" />
+          <Flashcard question="What are the key characteristics of a successful startup?" answer="shega" />
+          <Flashcard question="who are you?" answer="shega" />
+        </div>
+
+        <div onClick={handleLoadMore} className={`text-center pt-4 cursor-pointer opacity-80 ${isLoadMore? 'hidden' : ''}`}>Load more...</div>
+        <div onClick={handleLoadMore} className={`text-center pt-4 cursor-pointer opacity-80 ${isLoadMore? '' : 'hidden'}`}>Show less...</div>
       </div>
     </main >
   );
